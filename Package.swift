ebf5c040af7297f8758ca6edc7cb2f106e856f9d
Iskandar Abudiab <mail@iabudiab.dev>
2021-01-31 00:31:24 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+	name: "SwiftkubeServiceDiscovery",
+	platforms: [
+		.macOS(.v10_15)
+	],
+	products: [
+		.library(
+			name: "SwiftkubeServiceDiscovery",
+			targets: ["SwiftkubeServiceDiscovery"]),
+	],
+	dependencies: [
+		.package(name: "SwiftkubeClient", path: "../client"),
+		.package(url: "https://github.com/apple/swift-service-discovery.git", from: "0.1.0")
+	],
+	targets: [
+		.target(
+			name: "SwiftkubeServiceDiscovery",
+			dependencies: [
+				.product(name: "SwiftkubeClient", package: "SwiftkubeClient"),
+				.product(name: "ServiceDiscovery", package: "swift-service-discovery")
+			]),
+		.testTarget(
+			name: "SwiftkubeServiceDiscoveryTests",
+			dependencies: ["SwiftkubeServiceDiscovery"]),
+	]
+)
---
+++ README.md
@@ -0,0 +1,5 @@
+# Swiftkube:ServiceDiscovery
+
+An implementation of the [Swift Service Discovery API](https://github.com/apple/swift-service-discovery) for Kubernetes based on Swiftkube tooling.  
+
+This is a WIP
