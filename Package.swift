a47c4b5b7976ee10861dbb3efc7bafab2c61395d
Iskandar Abudiab <mail@iabudiab.dev>
2025-03-04 23:00:21 +0100

    Update Swift Tool version to 5.9 and macOS to v12

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
 	name: "SwiftkubeServiceDiscovery",
 	platforms: [
-		.macOS(.v10_15)
+		.macOS(.v12)
 	],
 	products: [
 		.library(
@@ -27,5 +27,6 @@ let package = Package(
 		.testTarget(
 			name: "SwiftkubeServiceDiscoveryTests",
 			dependencies: ["SwiftkubeServiceDiscovery"]),
-	]
+	],
+	swiftLanguageVersions: [.v5, .version("6")]
 )
